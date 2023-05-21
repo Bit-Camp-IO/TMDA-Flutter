075f910a1855a42b92acc1ecb7a196c5e113dbe3
Momen Muhammad <momenmuhammad01@gmail.com>
2023-05-21 18:55:21 +0300

    basic cubit impl

---
+++ lib/features/auth/presentation/cubit/auth_cubit.dart
@@ -0,0 +1,8 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'auth_state.dart';
+
+class AuthCubit extends Cubit<AuthState> {
+  AuthCubit() : super(AuthInitial());
+}
---
+++ lib/features/auth/presentation/cubit/auth_state.dart
@@ -0,0 +1,10 @@
+part of 'auth_cubit.dart';
+
+abstract class AuthState extends Equatable {
+  const AuthState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class AuthInitial extends AuthState {}

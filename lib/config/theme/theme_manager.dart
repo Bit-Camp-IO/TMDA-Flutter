01c02136e54ddcb3b6f1c88b647d93064377a970
Momen Muhammad <momenmuhammad01@gmail.com>
2023-05-21 18:54:12 +0300

    Theme Manager basic impl

---
+++ lib/config/theme/theme_manager.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ThemeManager{
+  static ThemeData lightTheme(){
+    return ThemeData();
+  }
+  static ThemeData darkTheme(){
+    return ThemeData();
+  }
+}

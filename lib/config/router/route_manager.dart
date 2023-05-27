3f61b4e3fc1c7c8fa297e02f3902689bb155326f
Momen Muhammad <momenmuhammad01@gmail.com>
2023-05-27 19:00:09 +0300

    Added Routes

--- lib/config/router/route_manager.dart
+++ lib/config/router/route_manager.dart
@@ -1,14 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tmda/core/constants/route_constants.dart';
 import 'package:tmda/core/widget/no_route_found.dart';
+import 'package:tmda/features/auth/presentation/cubit/auth_cubit.dart';
 import 'package:tmda/features/auth/presentation/screens/login_screen.dart';
+import 'package:tmda/features/movie/presentation/screens/movie_screen.dart';
 
 class RouteManager {
   static Route<dynamic> generateRoute(RouteSettings routeSettings) {
     switch (routeSettings.name) {
       case (initialRoute):
         return MaterialPageRoute(
-          builder: (context) => const LoginScreen(),
+          builder: (context) => BlocBuilder<AuthCubit, AuthState>(
+            builder: (context, state) {
+              if (state is AuthInitial && state.authData.requestSuccess!) {
+                return MovieScreen(sessionId: state.authData.sessionId!);
+              } else {
+                return const LoginScreen();
+              }
+            },
+          ),
+        );
+      case (movieRoute):
+        return MaterialPageRoute(
+          builder: (context) =>
+              MovieScreen(sessionId: routeSettings.arguments.toString()),
         );
       default:
         return MaterialPageRoute(
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:tmda/app.dart';
 import 'package:tmda/bloc_observer.dart';
 import 'package:tmda/injection_container.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
   await InjectionContainer().init();
   Bloc.observer = MyBlocObserver();
   runApp(const MyApp());

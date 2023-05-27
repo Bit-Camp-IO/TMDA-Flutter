22c56152d3dbce74b181ac6f9e70f6d2ed93968f
Momen Muhammad <momenmuhammad01@gmail.com>
2023-05-27 19:01:09 +0300

    modified base url

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,6 +1,6 @@
 const String apiKey = '45fe17b977fb94a0c0c099b894af2c74';
 const String apiKeyQuery = 'api_key';
-const String apiBaseUrl = 'https://api.themoviedb.org/3/';
+const String apiBaseUrl = 'https://api.themoviedb.org/3';
 const String apiHeaderContentTypeValue = 'application/json';
 const String apiHeaderContentType = 'Content-Type';
 const String apiHeaderAuthorization =  'Authorization';

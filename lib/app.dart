98cd9e6e22a9c9a5dfe2dd24957c8d47b5642f41
Momen Muhammad <momenmuhammad01@gmail.com>
2023-06-01 20:24:56 +0300

    Added screen util package

--- lib/app.dart
+++ lib/app.dart
@@ -1,31 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:tmda/config/router/route_manager.dart';
+import 'package:tmda/config/theme/theme_manager.dart';
 import 'package:tmda/features/auth/presentation/cubit/auth_cubit.dart';
-import 'package:tmda/features/movie/presentation/cubit/movie_cubit.dart';
+import 'package:tmda/features/movie/presentation/bloc/dot_indicator/dot_indicator_cubit.dart';
+import 'package:tmda/features/movie/presentation/bloc/movie_details/movie_details_cubit.dart';
+import 'package:tmda/features/movie/presentation/bloc/movies_bloc/movies_bloc.dart';
 import 'package:tmda/injection_container.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
         BlocProvider(
-          create: (context) => sl<AuthCubit>(),
+          create: (context) => sl<AuthCubit>()..checkUserLoggedIn(),
         ),
         BlocProvider(
-          create: (context) => sl<MovieCubit>(),
+          create: (context) => sl<MoviesBloc>()
+            ..add(GetNowPlayingMoviesEvent())
+            ..add(GetUpcomingMoviesEvent())
+            ..add(GetPopularMoviesEvent())
+            ..add(GetTopRatedMoviesEvent()),
         ),
+        BlocProvider(
+          create: (context) => sl<DotIndicatorCubit>(),
+        ),
+        BlocProvider(
+          create: (context) => sl<MovieDetailsCubit>(),
+        )
       ],
-      child: MaterialApp(
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      child: ScreenUtilInit(
+        designSize: const Size(360, 800),
+        minTextAdapt: true,
+        splitScreenMode: true,
+        builder: (context, child) => MaterialApp(
+          theme: ThemeManager.darkTheme(),
+          onGenerateRoute: RouteManager.generateRoute,
+          debugShowCheckedModeBanner: false,
         ),
-        onGenerateRoute: RouteManager.generateRoute,
-        debugShowCheckedModeBanner: false,
       ),
     );
   }

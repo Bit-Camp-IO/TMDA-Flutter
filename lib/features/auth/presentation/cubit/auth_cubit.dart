aa1ce9a36a67ef3511fb191403dcb4ae075d9808
Momen Muhammad <momenmuhammad01@gmail.com>
2023-06-01 20:30:59 +0300

    changed auth cubit impl

--- lib/features/auth/presentation/cubit/auth_cubit.dart
+++ lib/features/auth/presentation/cubit/auth_cubit.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:tmda/features/auth/domain/entities/auth.dart';
-import 'package:tmda/features/auth/domain/usecases/check_user_loging_session_usecase.dart';
+import 'package:tmda/features/auth/domain/usecases/check_user_login_session_usecase.dart';
 import 'package:tmda/features/auth/domain/usecases/user_login_usecase.dart';
 import 'package:tmda/features/auth/domain/usecases/user_register_usecase.dart';
 
@@ -14,30 +14,27 @@ class AuthCubit extends Cubit<AuthState> {
   UserLoginUseCase userLoginUseCase;
   CheckUserLoginSessionUseCase checkUserLoggedInUseCase;
   UserRegisterUseCase userRegisterUseCase;
-  late Auth authData;
   AuthCubit(
     this.userLoginUseCase,
     this.checkUserLoggedInUseCase,
     this.userRegisterUseCase,
-    this.authData,
-  ) : super(AuthInitial(authData));
+  ) : super(AuthInitialState());
 
   Future<void> userLogin(String username, String password) async{
-    emit(AuthLoading());
+    emit(AuthLoadingState());
     final response = await userLoginUseCase(username, password);
     debugPrint('bloc response>>>>> $response');
-    response.fold((l) => emit(AuthFailure()), (r) => emit(AuthSuccess(r)));
+    response.fold((l) => emit(AuthFailureState()), (r) => emit(AuthenticationState(r)));
   }
 
   Future<void> checkUserLoggedIn() async{
-    emit(AuthLoading());
     final response = await checkUserLoggedInUseCase();
     debugPrint('bloc response>>>>> $response');
-    response.fold((l) => emit(AuthFailure()), (r) => emit(AuthSuccess(r)));
+    response.fold((l) => emit(AuthFailureState()), (r) => emit(AuthenticationState(r)));
   }
 
   Future<void> userRegister() async{
     final response = await userRegisterUseCase();
-    response.fold((l) => emit(AuthFailure()), (r) => emit(AuthRegister()));
+    response.fold((l) => emit(AuthFailureState()), (r) => emit(AuthRegisterState()));
   }
 }

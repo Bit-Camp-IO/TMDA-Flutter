a2ffe06c8fa1a66a6046c7dec4d3518e5c01a46c
Momen Muhammad <momenmuhammad01@gmail.com>
2023-06-01 20:34:59 +0300

    dot indicator cubit impl

---
+++ lib/features/movie/presentation/bloc/dot_indicator/dot_indicator_state.dart
@@ -0,0 +1,12 @@
+part of 'dot_indicator_cubit.dart';
+
+class DotIndicatorState extends Equatable{
+  final int index;
+
+  const DotIndicatorState(this.index);
+
+
+  @override
+  List<Object> get props => [index];
+}
+

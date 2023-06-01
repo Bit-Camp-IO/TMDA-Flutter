d96a2f1eeb86ece600a0750c38d7b4cd88685730
Momen Muhammad <momenmuhammad01@gmail.com>
2023-06-01 20:27:18 +0300

    basic impl

--- lib/core/util/strings_manager.dart
+++ lib/core/util/strings_manager.dart
@@ -1,3 +1,12 @@
 class StringsManager {
+  // Core Strings
+  static const String appName = 'TMDA';
   static const String noRouteFound = 'We couldn\'t find this route';
+
+  // Login Strings
+
+  static const String loginUserName = 'Username';
+  static const String loginPassword = 'Password';
+  static const String noAccount = 'Don\'t have an account ?';
+  static const String register = 'Register';
 }

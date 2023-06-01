7fdf209c563b906f440d0f203d591d6f6f2fb121
Momen Muhammad <momenmuhammad01@gmail.com>
2023-06-01 20:25:46 +0300

    Added routes

--- lib/config/router/route_manager.dart
+++ lib/config/router/route_manager.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tmda/core/constants/route_constants.dart';
 import 'package:tmda/core/widget/no_route_found.dart';
+import 'package:tmda/features/auth/presentation/screens/splash_screen.dart';
 import 'package:tmda/features/auth/presentation/cubit/auth_cubit.dart';
 import 'package:tmda/features/auth/presentation/screens/login_screen.dart';
+import 'package:tmda/features/movie/presentation/screens/movie_details_screen.dart';
 import 'package:tmda/features/movie/presentation/screens/movie_screen.dart';
 
 class RouteManager {
@@ -13,19 +15,31 @@ class RouteManager {
         return MaterialPageRoute(
           builder: (context) => BlocBuilder<AuthCubit, AuthState>(
             builder: (context, state) {
-              if (state is AuthInitial && state.authData.requestSuccess!) {
-                return MovieScreen(sessionId: state.authData.sessionId!);
-              } else {
+              if (state is AuthenticationState &&
+                  state.authData.requestSuccess!) {
+                return const MovieScreen();
+              } else if (state is AuthenticationState &&
+                      !state.authData.requestSuccess! ||
+                  state is AuthFailureState ||
+                  state is AuthLoadingState) {
                 return const LoginScreen();
+              } else {
+                return const SplashScreen();
               }
             },
           ),
         );
+
+      case (loginRoute):
+        return MaterialPageRoute(
+          builder: (context) => const LoginScreen(),
+        );
       case (movieRoute):
         return MaterialPageRoute(
-          builder: (context) =>
-              MovieScreen(sessionId: routeSettings.arguments.toString()),
+          builder: (context) => const MovieScreen(),
         );
+      case(movieDetailsRoute):
+        return MaterialPageRoute(builder: (context) => MovieDetailsScreen(movieId: routeSettings.arguments.toString()));
       default:
         return MaterialPageRoute(
           builder: (context) => const NoRouteFound(),

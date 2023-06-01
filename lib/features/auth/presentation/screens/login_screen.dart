7ebd9061aadea9e191adb58c15cbeeea3185f889
Momen Muhammad <momenmuhammad01@gmail.com>
2023-06-01 20:31:58 +0300

    basic login screen impl

--- lib/features/auth/presentation/screens/login_screen.dart
+++ lib/features/auth/presentation/screens/login_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tmda/core/constants/route_constants.dart';
+import 'package:tmda/core/util/strings_manager.dart';
 import 'package:tmda/features/auth/presentation/cubit/auth_cubit.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -20,14 +22,20 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return BlocConsumer<AuthCubit, AuthState>(
       listener: (context, state) {
-        if (state is AuthLoading) {
+        if (state is AuthLoadingState) {
           loading = true;
-        }
-        if (state is AuthSuccess && state.authData.requestSuccess!) {
-          Navigator.of(context)
-              .pushReplacementNamed(movieRoute, arguments: state.authData.sessionId);
-        } else if (state is AuthFailure) {
-          debugPrint('Login Failed');
+        } else if (state is AuthenticationState &&
+            state.authData.requestSuccess!) {
+          Navigator.of(context).pushReplacementNamed(
+            movieRoute
+          );
+        } else if (state is AuthenticationState &&
+            !state.authData.requestSuccess!) {
+          loading = false;
+          ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text(state.authData.statusMessage!)));
+        } else if (state is AuthFailureState) {
+          debugPrint('There was a problem');
         }
       },
       builder: (context, state) {
@@ -37,7 +45,7 @@ class _LoginScreenState extends State<LoginScreen> {
             backgroundColor: const Color(0xFF19191B),
             elevation: 0,
             title: const Text(
-              'Login',
+              StringsManager.appName,
               style: TextStyle(color: Colors.white),
             ),
             centerTitle: true,
@@ -48,73 +56,79 @@ class _LoginScreenState extends State<LoginScreen> {
               key: formKey,
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  TextFormField(
-                    style: const TextStyle(color: Colors.white),
-                    onSaved: (newValue) {
-                      if (newValue != null) {
-                        username = newValue;
-                      }
-                    },
-                    decoration: const InputDecoration(
-                        labelText: 'Username',
-                        labelStyle: TextStyle(color: Colors.white),
-                        border: OutlineInputBorder()),
-                  ),
-                  const SizedBox(height: 50),
-                  TextFormField(
-                    obscureText: true,
-                    onSaved: (newValue) {
-                      if (newValue != null) {
-                        password = newValue;
-                      }
-                    },
-                    style: const TextStyle(color: Colors.white),
-                    decoration: const InputDecoration(
-                        labelText: 'Password',
-                        labelStyle: TextStyle(color: Colors.white),
-                        border: OutlineInputBorder()),
-                  ),
-                  const SizedBox(height: 30),
-                  ElevatedButton(
-                    onPressed: () async {
-                      FormState? loginData = formKey.currentState;
-                      if (loginData!.validate()) {
-                        loginData.save();
-                      }
-                      await BlocProvider.of<AuthCubit>(context)
-                          .userLogin(username, password);
-                    },
-                    style: const ButtonStyle(
-                      fixedSize: MaterialStatePropertyAll(
-                        Size(300, 70),
-                      ),
+                children: AnimateList(
+                  interval: 250.ms,
+                  effects: [FadeEffect(duration: 150.ms)],
+                  children: [
+                    TextFormField(
+                      style: const TextStyle(color: Colors.white),
+                      onSaved: (newValue) {
+                        if (newValue != null) {
+                          username = newValue;
+                        }
+                      },
+                      decoration: const InputDecoration(
+                          labelText: StringsManager.loginUserName,
+                          labelStyle: TextStyle(color: Colors.white),
+                          border: OutlineInputBorder()),
                     ),
-                    child: loading
-                        ? const CircularProgressIndicator(color: Colors.white,)
-                        : const Text(
-                            'Login',
-                            style: TextStyle(fontSize: 24),
-                          ),
-                  ),
-                  const SizedBox(height: 50),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Text(
-                        'Don\'t have account ?',
-                        style: TextStyle(color: Colors.white),
+                    const SizedBox(height: 50),
+                    TextFormField(
+                      obscureText: true,
+                      onSaved: (newValue) {
+                        if (newValue != null) {
+                          password = newValue;
+                        }
+                      },
+                      style: const TextStyle(color: Colors.white),
+                      decoration: const InputDecoration(
+                          labelText: StringsManager.loginPassword,
+                          labelStyle: TextStyle(color: Colors.white),
+                          border: OutlineInputBorder()),
+                    ),
+                    const SizedBox(height: 30),
+                    ElevatedButton(
+                      onPressed: () async {
+                        FormState? loginData = formKey.currentState;
+                        if (loginData!.validate()) {
+                          loginData.save();
+                        }
+                        await BlocProvider.of<AuthCubit>(context)
+                            .userLogin(username, password);
+                      },
+                      style: const ButtonStyle(
+                        fixedSize: MaterialStatePropertyAll(
+                          Size(300, 70),
+                        ),
                       ),
-                      TextButton(
-                        onPressed: () async {
-                          await BlocProvider.of<AuthCubit>(context)
-                              .userRegister();
-                        },
-                        child: const Text('Register Now'),
-                      ),
-                    ],
-                  ),
-                ],
+                      child: loading
+                          ? const CircularProgressIndicator(
+                              color: Colors.white,
+                            )
+                          : const Text(
+                              'Login',
+                              style: TextStyle(fontSize: 24),
+                            ),
+                    ),
+                    const SizedBox(height: 50),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text(
+                          StringsManager.noAccount,
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        TextButton(
+                          onPressed: () async {
+                            await BlocProvider.of<AuthCubit>(context)
+                                .userRegister();
+                          },
+                          child: const Text(StringsManager.register),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
           ),

0bcb4536a96e39bcaf7c41a6f07ca5896df60c8a
Momen Muhammad <momenmuhammad01@gmail.com>
2023-06-01 20:35:49 +0300

    changed movie cubit to movies bloc

--- lib/features/movie/presentation/cubit/movie_cubit.dart
+++
@@ -1,8 +0,0 @@
-import 'package:equatable/equatable.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-
-part 'movie_state.dart';
-
-class MovieCubit extends Cubit<MovieState> {
-  MovieCubit() : super(MovieInitial());
-}

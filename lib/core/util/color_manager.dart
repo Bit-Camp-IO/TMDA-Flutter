d531186821a17dd8befab89f56b28950bf29494a
Momen Muhammad <momenmuhammad01@gmail.com>
2023-06-01 20:26:54 +0300

    colors manager impl

--- lib/core/util/color_manager.dart
+++ lib/core/util/color_manager.dart
@@ -1,1 +1,8 @@
-class ColorsManager{}
+import 'package:flutter/material.dart';
+
+class ColorsManager{
+  static const Color primaryColor = Color(0xFF00FFFF);
+  static const Color secondaryColor = Color(0xFFFE53BB);
+  static const Color backgroundColor = Color(0xFF000000);
+  static const Color errorColor = Color(0xFFDA0A0A);
+}

0de56030d4aea80d9fc51252c5dac21e69780712
Momen Muhammad <momenmuhammad01@gmail.com>
2023-06-01 20:23:29 +0300

    changed default font

--- lib/core/util/assets_manager.dart
+++ lib/core/util/assets_manager.dart
@@ -1,1 +1,7 @@
-class AssetsManager{}
+class AssetsManager{
+  static const fontFamily = 'Poppins';
+  static const appLogo = 'assets/images/app_logo.png';
+  static const neonBackground = 'assets/images/neon_background.png';
+  static const testImage= 'assets/images/test_image.jpg';
+  static const testTvImage = 'assets/images/tv_test_image.jpg';
+}

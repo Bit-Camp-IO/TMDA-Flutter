0cce8499042ac50a147c94845bb64bb40df8cc1c
Momen Muhammad <momenmuhammad01@gmail.com>
2023-06-01 20:26:14 +0300

    basic impl

--- lib/config/theme/theme_manager.dart
+++ lib/config/theme/theme_manager.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:tmda/core/util/assets_manager.dart';
-import 'package:tmda/core/util/color_manager.dart';
 
 class ThemeManager{
   static ThemeData darkTheme(){
     return ThemeData(
       scaffoldBackgroundColor: Colors.white,
       fontFamily: AssetsManager.fontFamily
-      
     );
   }
 }

139ecef06652b664136fd33f3bb7c98799a2b911
Momen Muhammad <momenmuhammad01@gmail.com>
2023-06-03 00:19:30 +0300

    removed unnecessary assets

--- lib/core/util/assets_manager.dart
+++ lib/core/util/assets_manager.dart
@@ -1,7 +1,4 @@
 class AssetsManager{
   static const fontFamily = 'Poppins';
   static const appLogo = 'assets/images/app_logo.png';
-  static const neonBackground = 'assets/images/neon_background.png';
-  static const testImage= 'assets/images/test_image.jpg';
-  static const testTvImage = 'assets/images/tv_test_image.jpg';
 }

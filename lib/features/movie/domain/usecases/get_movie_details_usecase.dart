b5b6be1246177eb4ac27ab9fb824cd97a78f52b0
Momen Muhammad <momenmuhammad01@gmail.com>
2023-06-06 04:55:22 +0300

    removed unnecessary rules

--- lib/features/movie/domain/usecases/get_movie_details_usecase.dart
+++ lib/features/movie/domain/usecases/get_movie_details_usecase.dart
@@ -1,4 +1,3 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:dartz/dartz.dart';
 import 'package:tmda/core/error/failure.dart';
 import 'package:tmda/features/movie/domain/entities/movie_details.dart';

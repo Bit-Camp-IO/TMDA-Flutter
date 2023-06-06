f748676821bdcab4fe286f8699df263b471af952
Momen Muhammad <momenmuhammad01@gmail.com>
2023-06-06 04:47:08 +0300

    Added new custom icons

---
+++ lib/core/icons/solar_system_icons.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/widgets.dart';
+
+class SolarSystemIcons {
+  SolarSystemIcons._();
+
+  static const _kFontFam = 'SolarSystem';
+  static const String? _kFontPkg = null;
+
+  static const IconData birthday = IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData favorite = IconData(0xe801, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData iosBackArrow = IconData(0xe802, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData movie = IconData(0xe803, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData outlineMovie = IconData(0xe804, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData person = IconData(0xe805, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData search = IconData(0xe806, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData signOut = IconData(0xe807, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData tv = IconData(0xe808, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData visibility = IconData(0xe809, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData visibilityOff = IconData(0xe80a, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+}

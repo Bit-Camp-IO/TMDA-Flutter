8baa5725f910e00677f9459f95d8a587ec060cb0
Momen Muhammad <momenmuhammad01@gmail.com>
2023-06-06 04:56:36 +0300

    initial impl

---
+++ lib/features/search/presentation/cubit/search_cubit.dart
@@ -0,0 +1,9 @@
+
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'search_state.dart';
+
+class SearchCubit extends Cubit<SearchState> {
+  SearchCubit() : super(SearchInitial());
+}

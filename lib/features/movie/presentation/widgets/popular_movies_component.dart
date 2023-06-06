29663ed386298e85257ab72f16e22a061f972203
Momen Muhammad <momenmuhammad01@gmail.com>
2023-06-06 04:56:03 +0300

    converted to stateless widget

--- lib/features/movie/presentation/widgets/popular_movies_component.dart
+++ lib/features/movie/presentation/widgets/popular_movies_component.dart
@@ -1,19 +1,16 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:tmda/config/router/route_manager.dart';
+import 'package:tmda/config/router/app_router.dart';
 import 'package:tmda/core/util/enums.dart';
 import 'package:tmda/core/widget/custom_poster_card.dart';
 import 'package:tmda/features/movie/presentation/bloc/movies_bloc/movies_bloc.dart';
 
-class PopularMoviesComponent extends StatefulWidget {
+class PopularMoviesComponent extends StatelessWidget {
   const PopularMoviesComponent({super.key});
 
-  @override
-  State<PopularMoviesComponent> createState() => _PopularMoviesComponentState();
-}
-
-class _PopularMoviesComponentState extends State<PopularMoviesComponent> {
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<MoviesBloc, MoviesState>(
@@ -22,9 +19,9 @@ class _PopularMoviesComponentState extends State<PopularMoviesComponent> {
       builder: (context, state) {
         switch (state.popularState) {
           case BlocStateEnum.loading:
-            return const SizedBox(
-              height: 250,
-              child: Center(
+            return SizedBox(
+              height: 270.h,
+              child: const Center(
                 child: CircularProgressIndicator(
                   color: Colors.white,
                 ),
@@ -32,32 +29,39 @@ class _PopularMoviesComponentState extends State<PopularMoviesComponent> {
             );
           case BlocStateEnum.loaded:
             return SizedBox(
-              height: 270.h,
-              child: ListView.builder(
-                itemCount: state.popularMovies.length,
-                scrollDirection: Axis.horizontal,
-                physics: const BouncingScrollPhysics(),
-                shrinkWrap: true,
-                itemBuilder: (context, index) {
-                  return Row(
-                    children: [
-                      SizedBox(width: 24.w),
-                      CustomPosterCard(
-                        onTap: () {
-                          Navigator.of(context).pushNamed(
-                              AppRouter.movieDetailsRoute,
-                              arguments: state.popularMovies[index].movieId,
+              height: 280.h,
+              child: Animate(
+                effects: [FadeEffect(duration: 250.ms)],
+                child: ListView.builder(
+                  itemCount: state.popularMovies.length,
+                  scrollDirection: Axis.horizontal,
+                  physics: const BouncingScrollPhysics(),
+                  shrinkWrap: true,
+                  itemBuilder: (context, index) {
+                    return Row(
+                      children: [
+                        SizedBox(width: 24.w),
+                        CustomPosterCard(
+                          onTap: () {
+                            context.pushRoute(
+                              MovieDetailsRoute(
+                                movieId: state.popularMovies[index].movieId
+                                    .toString(),
+                              ),
                             );
-                        },
-                        title: state.popularMovies[index].movieTitle!,
-                        imagePath: state.popularMovies[index].moviePosterPath!,
-                        releaseYear: state.popularMovies[index].movieReleaseDate!
-                            .substring(0, 4),
-                        rating: state.popularMovies[index].movieVote,
-                      ),
-                    ],
-                  );
-                },
+                          },
+                          title: state.popularMovies[index].movieTitle!,
+                          imagePath:
+                              state.popularMovies[index].moviePosterPath!,
+                          releaseYear: state
+                              .popularMovies[index].movieReleaseDate!
+                              .substring(0, 4),
+                          rating: state.popularMovies[index].movieVote,
+                        ),
+                      ],
+                    );
+                  },
+                ),
               ),
             );
           case BlocStateEnum.error:

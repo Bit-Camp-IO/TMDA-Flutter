8214301978aacfaaf972a01750c98dc0c16bfa17
Momen Muhammad <momenmuhammad01@gmail.com>
2023-06-13 00:51:52 +0300

    Added MovieType enum

--- lib/core/util/enums.dart
+++ lib/core/util/enums.dart
@@ -1,5 +1,15 @@
-enum BlocStateEnum{
+enum MovieType {
+  newMovies('New Movies'),
+  popularMovies('Popular Movies'),
+  topRatedMovies('Top Rated Movies'),
+  moreLikeThisMovies('Similar Movies');
+
+  const MovieType(this.name);
+  final String name;
+}
+
+enum BlocState {
   loading,
-  loaded,
-  error,
+  success,
+  failure,
 }

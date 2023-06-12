a266b7231fbe441981863246c201219710e6441d
Momen Muhammad <momenmuhammad01@gmail.com>
2023-06-13 01:00:40 +0300

    separated auth cubits in a route wrapper

---
+++ lib/features/auth/presentation/screens/auth_wrapper.dart
@@ -0,0 +1,26 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tmda/features/auth/presentation/cubit/auth_cubit.dart';
+import 'package:tmda/features/auth/presentation/cubit/login_cubit.dart';
+import 'package:tmda/injection_container.dart';
+
+@RoutePage()
+class AuthWrapperScreen extends StatelessWidget {
+  const AuthWrapperScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => sl<AuthCubit>()..checkUserLoggedIn(),
+        ),
+        BlocProvider(
+          create: (context) => sl<LoginCubit>(),
+        ),
+      ],
+      child: const AutoRouter(),
+    );
+  }
+}

afcc86ec61a7b3429ff476fcd2e0426997bc6ce2
Momen Muhammad <momenmuhammad01@gmail.com>
2023-06-13 00:48:47 +0300

    initial impl

---
+++ lib/config/theme/size_config.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class SizeConfig {
+  static late MediaQueryData _mediaQueryData;
+  static late double screenWidth;
+  static late double screenHeight;
+  static double? defaultSize;
+  static Orientation? orientation;
+
+  void init(BuildContext context) {
+    _mediaQueryData = MediaQuery.of(context);
+    screenWidth = _mediaQueryData.size.width;
+    screenHeight = _mediaQueryData.size.height;
+    orientation = _mediaQueryData.orientation;
+  }
+}
+
+double getProportionateScreenHeight(double inputHeight) {
+  double screenHeight = SizeConfig.screenHeight;
+  return (inputHeight / 812.0) * screenHeight;
+}
+
+double getProportionateScreenWidth(double inputHeight) {
+  double screenWidth = SizeConfig.screenWidth;
+  return (inputHeight / 375.0) * screenWidth;
+}

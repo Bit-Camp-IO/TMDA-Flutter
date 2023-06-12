038604100da16db31bb4a59be73f18eb9e088ff9
Momen Muhammad <momenmuhammad01@gmail.com>
2023-06-13 00:46:13 +0300

    multi bloc provider removed

--- lib/app.dart
+++ lib/app.dart
@@ -1,12 +1,8 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:tmda/config/router/app_router.dart';
 import 'package:tmda/config/theme/theme_manager.dart';
-import 'package:tmda/features/auth/presentation/cubit/auth_cubit.dart';
-import 'package:tmda/features/movie/presentation/bloc/dot_indicator/dot_indicator_cubit.dart';
-import 'package:tmda/features/movie/presentation/bloc/movie_details/movie_details_cubit.dart';
-import 'package:tmda/features/movie/presentation/bloc/movies_bloc/movies_bloc.dart';
 import 'package:tmda/injection_container.dart';
 import 'package:tmda/route_observer.dart';
 
@@ -14,36 +10,19 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(
-          create: (context) => sl<AuthCubit>()..checkUserLoggedIn(),
+    return ScreenUtilInit(
+      designSize: const Size(360, 800),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) => MaterialApp.router(
+        debugShowCheckedModeBanner: false,
+        routerDelegate: AutoRouterDelegate(
+          navigatorObservers: () => [MyRouteObserver()],
+          sl<AppRouter>(),
         ),
-        BlocProvider(
-          create: (context) => sl<MoviesBloc>()
-            ..add(GetNowPlayingMoviesEvent())
-            ..add(GetNewMoviesEvent())
-            ..add(GetPopularMoviesEvent())
-            ..add(GetTopRatedMoviesEvent()),
-        ),
-        BlocProvider(
-          create: (context) => sl<DotIndicatorCubit>(),
-        ),
-        BlocProvider(
-          create: (context) => sl<MovieDetailsCubit>(),
-        )
-      ],
-      child: ScreenUtilInit(
-        designSize: const Size(360, 800),
-        minTextAdapt: true,
-        splitScreenMode: true,
-        builder: (context, child) => MaterialApp.router(
-          debugShowCheckedModeBanner: false,
-          routerConfig: sl<AppRouter>().config(
-            navigatorObservers: () => [MyRouteObserver()],
-          ),
-          theme: ThemeManager.darkTheme(),
-        ),
+        routeInformationParser:
+            sl<AppRouter>().defaultRouteParser(includePrefixMatches: true),
+        theme: ThemeManager.darkTheme(),
       ),
     );
   }

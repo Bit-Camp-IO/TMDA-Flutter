bededf08697b7a52878524cf01f818ac5dcb315f
Momen Muhammad <momenmuhammad01@gmail.com>
2023-06-13 00:55:35 +0300

    base impl

--- lib/core/widget/custom_icon_button.dart
+++ lib/core/widget/custom_icon_button.dart
@@ -16,8 +16,9 @@ class CustomIconButton extends StatelessWidget {
       height: 40.h,
       width: 40.w,
       decoration: BoxDecoration(
-          color: Colors.black.withOpacity(0.37),
-          borderRadius: BorderRadius.all(const Radius.circular(10).w)),
+        color: Colors.black.withOpacity(0.37),
+        borderRadius: BorderRadius.all(const Radius.circular(10).w),
+      ),
       child: IconButton(
         onPressed: onPressed,
         icon: Icon(

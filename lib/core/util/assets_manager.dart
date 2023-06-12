f8cf7dfb2ee29572fc939385b282c06338de9c7e
Momen Muhammad <momenmuhammad01@gmail.com>
2023-06-13 00:51:26 +0300

    added new assets

--- lib/core/util/assets_manager.dart
+++ lib/core/util/assets_manager.dart
@@ -1,5 +1,14 @@
-class AssetsManager{
+class AssetsManager {
+  // Fonts Assets
   static const fontFamily = 'Poppins';
+
+  // Image Assets
   static const appLogo = 'assets/images/app_logo.svg';
   static const neonButton = 'assets/images/neon_button.png';
+  static const neonPlayButton = 'assets/images/play_button.svg';
+  static const actorNoPic =
+      'https://media.istockphoto.com/id/1327592506/vector/default-avatar-photo-placeholder-icon-grey-profile-picture-business-man.jpg?s=612x612&w=0&k=20&c=BpR0FVaEa5F24GIw7K8nMWiiGmbb8qmhfkpXcp1dhQg=';
+  static const noPoster =
+      'https://cdn.dribbble.com/users/55871/screenshots/2158022/media/8f2a4a2c9126a9f265fb9e1023b1698a.jpg?compress=1&resize=800x600&vertical=center';
+  
 }

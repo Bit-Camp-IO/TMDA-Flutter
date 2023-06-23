2c666e1286b97032d628708c7e50beb2d4c6d061
Momen Muhammad <momenmuhammad01@gmail.com>
2023-06-23 12:37:53 +0300

    Added Tv Show Enum

--- lib/core/util/enums.dart
+++ lib/core/util/enums.dart
@@ -2,9 +2,20 @@ enum MovieType {
   newMovies('New Movies'),
   popularMovies('Popular Movies'),
   topRatedMovies('Top Rated Movies'),
-  moreLikeThisMovies('Similar Movies');
+  moreMoviesLikeThis('Similar Movies');
 
   const MovieType(this.name);
+  final String name;
+}
+
+enum TvShowType {
+  topRatedTvShows('Top Rated Shows'),
+  airingThisWeek('Airing This week'),
+  popularTvShows('Popular Shows'),
+  recommendedTvShows('Recommended Tv Shows'),
+  similarTvShows('Similar Shows');
+
+  const TvShowType(this.name);
   final String name;
 }
 

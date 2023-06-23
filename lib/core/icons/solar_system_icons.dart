7c9a7ba4c86d6554792452145c28b93fcf509310
Momen Muhammad <momenmuhammad01@gmail.com>
2023-06-23 12:31:40 +0300

    Added new icons

--- lib/core/icons/solar_system_icons.dart
+++ lib/core/icons/solar_system_icons.dart
@@ -11,8 +11,7 @@ class SolarSystemIcons {
   static const IconData back_arrow_android = IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
   static const IconData birthday = IconData(0xe801, fontFamily: _kFontFam, fontPackage: _kFontPkg);
   static const IconData eyeslash = IconData(0xe802, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData favorite = IconData(0xe803, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData favorite_outlined = IconData(0xe804, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData wifi = IconData(0xe803, fontFamily: _kFontFam, fontPackage: _kFontPkg);
   static const IconData back_arrow_ios = IconData(0xe805, fontFamily: _kFontFam, fontPackage: _kFontPkg);
   static const IconData movie = IconData(0xe806, fontFamily: _kFontFam, fontPackage: _kFontPkg);
   static const IconData movie_outlined = IconData(0xe807, fontFamily: _kFontFam, fontPackage: _kFontPkg);
@@ -23,4 +22,6 @@ class SolarSystemIcons {
   static const IconData tv = IconData(0xe80c, fontFamily: _kFontFam, fontPackage: _kFontPkg);
   static const IconData visibility = IconData(0xe80d, fontFamily: _kFontFam, fontPackage: _kFontPkg);
   static const IconData visibility_off = IconData(0xe80e, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData save = IconData(0xe80f, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData save_outlined = IconData(0xe810, fontFamily: _kFontFam, fontPackage: _kFontPkg);
 }

30c9056cba5b172463a3c37d288691092972d583
Momen Muhammad <momenmuhammad01@gmail.com>
2023-06-23 12:42:44 +0300

    Added new strings

--- lib/core/util/strings_manager.dart
+++ lib/core/util/strings_manager.dart
@@ -11,7 +11,7 @@ class StringsManager {
   static const String tv = 'TV';
   static const String search = 'Search';
   static const String account = 'Account';
-  
+
   //! Auth Strings
   static const String login = 'Login';
   static const String register = 'Register';
@@ -20,33 +20,34 @@ class StringsManager {
   static const String registerNow = 'Register Now';
   static const String noAccount = 'Don\'t have an account ?';
   static const String forgetPassword = 'Forget Password?';
-  static const String overview = 'Your guide to movies, TV shows\n and celebrities';
+  static const String overview =
+      'Your guide to movies, TV shows\n and celebrities';
   static const String loginWelcome = 'Welcome to our\n community';
 
   //! Movie Feature Strings
-  // Core Strings
-  static const String movieUnknown = 'Unknown';
-
   // Home Screen
   static const String newMoviesSectionTitle = 'New movies';
   static const String popularMoviesSectionTitle = 'Popular movies';
   static const String topRatedMoviesSectionTitle = 'Top Rated movies';
   static const String seeAllMoviesButtonTitle = 'See All';
-
-  // Details Screen
-  static const String movieDetailsMaxRate = '/10';
-  static const String movieDetailsAddToWatchlist = 'Add';
-  static const String movieDetailsRemoveFromWatchList = 'Remove';
-  static const String movieDetailsProductionYear = 'Year';
-  static const String movieDetailsProductionCountry = 'Country';
-  static const String movieDetailsLength = 'Length';
-  static const String movieDetailsCastSection = 'Cast';
-  static const String movieDetailsDuration = 'Min';
-
-  static const String movieDetailsSimilarSectionTitle = 'More Like this';
+  static const String movieDetailsSimilarSectionTitle = 'Similar';
   static const String movieDetailsUserReviews = 'User Reviews';
   static const String movieDetailsReviewTitle = 'Review By';
   static const String movieDetailsNoRating = 'No rating';
-  static const String movieDetailsAlertContent = 'There is no videos for this movie.';
+  static const String movieDetailsAlertContent =
+      'There is no videos for this movie.';
   static const String movieDetailsAlertButtonTitle = 'close';
+
+  //! Shared
+  static const String maxRate = '/10';
+  static const String addToWatchlist = 'Add';
+  static const String removeFromWatchList = 'Remove';
+  static const String productionYear = 'Year';
+  static const String airedTitle = 'Aired';
+  static const String productionCountry = 'Country';
+  static const String length = 'Length';
+  static const String castSectionTitle = 'Cast';
+  static const String duration = 'Min';
+  static const String unknown = 'Unknown';
+  static const String from = 'From';
 }

bcede942e2cae54d4cfe0a0f5c651425202b2898
Momen Muhammad <momenmuhammad01@gmail.com>
2023-06-28 17:12:55 +0200

    fixed lottie import

--- lib/core/util/assets_manager.dart
+++ lib/core/util/assets_manager.dart
@@ -6,7 +6,7 @@ class AssetsManager {
   static const appLogo = 'assets/images/app_logo.png';
   static const neonButton = 'assets/images/neon_button.png';
   static const neonPlayButton = 'assets/images/play_button.svg';
-  static const neonLoading = 'assets/lottie/neon_loading';
+  static const neonLoading = 'assets/lottie/neon_loading.json';
   static const actorNoPic =
       'https://media.istockphoto.com/id/1327592506/vector/default-avatar-photo-placeholder-icon-grey-profile-picture-business-man.jpg?s=612x612&w=0&k=20&c=BpR0FVaEa5F24GIw7K8nMWiiGmbb8qmhfkpXcp1dhQg=';
   static const noPoster =

171b72532d65c9895c4e6ae4706a83b2f0424cce
Momen Muhammad <momenmuhammad01@gmail.com>
2023-06-28 17:13:12 +0200

    changed some colors

--- lib/core/util/color_manager.dart
+++ lib/core/util/color_manager.dart
@@ -5,7 +5,7 @@ class ColorsManager{
   static const Color secondaryColor = Color(0xFFFE53BB);
   static const Color backgroundColor = Color(0xFF000000);
   static const Color inActiveColor = Color(0xFFB1AEAE);
-  static const Color starsColor = Color(0xFFD1BC00);
+  static const Color ratingIconColor = Color(0xFFD1BC00);
   static const Color errorColor = Color(0xFFDA0A0A);
   static const Color darkPrimary = Color(0xFF007373);
   static const Color bottomNavigationColor = Color(0xFF343434);

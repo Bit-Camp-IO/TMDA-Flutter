4372a9db8d4ad9ade94755e378de981260532387
Momen Muhammad <momenmuhammad01@gmail.com>
2023-06-30 23:29:04 +0200

    Image rehandeld

--- lib/core/widgets/see_all_watchlist_card.dart
+++ lib/core/widgets/see_all_watchlist_card.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:tmda/core/icons/solar_system_icons.dart';
 import 'package:tmda/core/util/color_manager.dart';
 import 'package:tmda/core/util/strings_manager.dart';
 import 'package:tmda/core/widgets/tilted_image.dart';
@@ -16,6 +16,7 @@ class SeeAllWatchListCard extends StatelessWidget {
     required this.genres,
     required this.releaseYear,
     required this.language,
+    required this.errorImagePath,
   });
 
   final String title;
@@ -26,93 +27,97 @@ class SeeAllWatchListCard extends StatelessWidget {
   final List genres;
   final String language;
   final void Function() onTap;
-
+  final String errorImagePath;
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: onTap,
-      child: Transform(
-        transform: Matrix4.skewX(-0.06),
-        child: Container(
-          width: 312,
-          height: 210,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(30),
-            color: Colors.black.withOpacity(0.3),
-          ),
-          child: Stack(
-            children: [
-              Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: TiltedImage(
-                      imagePath: posterPath,
-                      width: 120.w,
-                      height: 170.h,
+    return Animate(
+      effects: [FadeEffect(duration: 400.ms)],
+      child: InkWell(
+        onTap: onTap,
+        child: Transform(
+          transform: Matrix4.skewX(-0.06),
+          child: Container(
+            width: 312,
+            height: 210,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              color: Colors.black.withOpacity(0.3),
+            ),
+            child: Stack(
+              children: [
+                Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: TiltedImage(
+                        errorImagePath: errorImagePath,
+                        imagePath: posterPath,
+                        width: 120.w,
+                        height: 170.h,
+                      ),
                     ),
-                  ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        title.length > 15
-                            ? '${title.substring(0, 15)}..'
-                            : title,
-                        style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                          fontWeight: FontWeight.w500,
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          title.length > 15
+                              ? '${title.substring(0, 15)}..'
+                              : title,
+                          style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                            fontWeight: FontWeight.w500,
+                          ),
                         ),
-                      ),
-                      SizedBox(height: 4.h),
-                      Row(
-                        children: [
-                          Text(
-                            releaseYear.isEmpty
-                                ? StringsManager.unknown
-                                : '${releaseYear.substring(0, 4)} ‧ ',
-                            style: Theme.of(context).textTheme.bodySmall,
-                          ),
-                          Text(
-                            _buildGenres(genres),
-                            style: Theme.of(context).textTheme.bodySmall,
-                          ),
-                          Text(
-                            language.toUpperCase(),
-                            style: Theme.of(context).textTheme.bodySmall,
-                          ),
-                        ],
-                      ),
-                      SizedBox(height: 7.h),
-                      Row(
-                        children: [
-                          const Icon(
-                            Icons.star,
-                            color: ColorsManager.ratingIconColor,
-                            size: 16,
-                          ),
-                          Text(
-                            vote.toString(),
-                            style: Theme.of(context)
-                                .textTheme
-                                .bodySmall!
-                                .copyWith(fontSize: 11),
-                          ),
-                          SizedBox(width: 20.w),
-                          Text(
-                            '$voteCount Votes',
-                            style: Theme.of(context)
-                                .textTheme
-                                .bodySmall!
-                                .copyWith(fontSize: 11),
-                          )
-                        ],
-                      )
-                    ],
-                  )
-                ],
-              ),
-            ],
+                        SizedBox(height: 4.h),
+                        Row(
+                          children: [
+                            Text(
+                              releaseYear.isEmpty
+                                  ? StringsManager.unknown
+                                  : '${releaseYear.substring(0, 4)} ‧ ',
+                              style: Theme.of(context).textTheme.bodySmall,
+                            ),
+                            Text(
+                              _buildGenres(genres),
+                              style: Theme.of(context).textTheme.bodySmall,
+                            ),
+                            Text(
+                              language.toUpperCase(),
+                              style: Theme.of(context).textTheme.bodySmall,
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 7.h),
+                        Row(
+                          children: [
+                            const Icon(
+                              Icons.star,
+                              color: ColorsManager.ratingIconColor,
+                              size: 16,
+                            ),
+                            Text(
+                              vote.toString(),
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .bodySmall!
+                                  .copyWith(fontSize: 11),
+                            ),
+                            SizedBox(width: 20.w),
+                            Text(
+                              '$voteCount Votes',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .bodySmall!
+                                  .copyWith(fontSize: 11),
+                            )
+                          ],
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
